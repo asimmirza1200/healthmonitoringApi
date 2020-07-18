fd90541a2a3371187262326b532d2a7de17a350b
asimmirza1200 <36633775+asimmirza1200@users.noreply.github.com>
2020-07-18 13:11:11 +0500

    Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -18,9 +18,18 @@ const app= express()
 app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
 const PORT =process.env.PORT || 3000
 app.listen(PORT, () =>
     console.log(`Helath Monitoring app listening on port ${PORT}!`)
 
 )
+ app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use("",doctorRoute)

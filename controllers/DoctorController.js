3112a603821ebd449f6f20a2810e8a8e1f64b96a
asimmirza1200 <36633775+asimmirza1200@users.noreply.github.com>
2020-07-18 14:13:07 +0500

    add doctor

--- controllers/DoctorController.js
+++ controllers/DoctorController.js
@@ -120,9 +120,7 @@ const loginDoctor =  (req, res,next) =>{
 };
 
 const insertDoctor =  (req, res,next) =>{
-     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    
     let doctor= new Doctor({
         doctorname: req.body.doctorname, 
         fathername: req.body.fathername,
